7aa9b9fb98ca3ecd5e430c0d48db603b6e3596de
Vladimir Dimitrov <vl.dimitrov89@gmail.com>
2016-03-13 12:34:36 +0200

    Added forms

--- index.php
+++ index.php
@@ -3,16 +3,24 @@
     <head>
         <meta charset="UTF-8">
         <title>Login</title>
-        <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     </head>
     <body>
         <form method="post">
-            <div class="input-group">
-                <span class="input-group-addon" id="basic-addon1">@</span>
-                <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="username">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1">@</span>
+                        <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="username">
+                    </div>
+
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1">P</span>
+                        <input type="text" class="form-control" placeholder="Password" aria-describedby="basic-addon1" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-default">Login</button>
+                </div>
             </div>
-            Password: <input type="text" name="password" />
-            <input type="submit" value="LogIn" />
         </form>
     </body>
 </html>
