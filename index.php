ccb06deb04a092cf5c9e0506a03ef7406f895ac0
Vladimir Dimitrov <vl.dimitrov89@gmail.com>
2016-03-13 12:18:40 +0200

    Added some changes

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     </head>
     <body>
         <form method="post">
-        Username: <input type="text" name="username" />
-        Password: <input type="text" name="password" />
-        <input type="submit" value="LogIn" />
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">@</span>
+                <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="username">
+            </div>
+            Password: <input type="text" name="password" />
+            <input type="submit" value="LogIn" />
         </form>
     </body>
 </html>
