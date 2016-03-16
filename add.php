96cdd559c69340a1e98e36bfbf1e087103808b51
Vladimir Dimitrov <vl.dimitrov89@gmail.com>
2016-03-16 22:00:10 +0200

    Completed the Adress Book

--- add.php
+++ add.php
@@ -2,7 +2,31 @@
  session_start();
  if ($_SESSION['is_Logged']==true)
  {
-     
+     if($_POST['formSubmit']==1)
+     {
+         $name = trim($_POST['name']);
+         $email = trim($_POST['mail']);
+         $phone = trim($_POST['phone']);
+         if(strlen($name)>3 && strlen($email)>5)
+         {
+             $tmp='name:'.$name.';email:'.$email.';phone:'.$phone.';';
+             file_put_contents('data.txt', $tmp."\r\n",FILE_APPEND);
+             echo 'Data saved!';
+         }
+         else
+         {
+             echo 'Wrong data!';
+         }
+     }
+     ?>
+<form action="add.php" method="post">
+    <input type="hidden" name="formSubmit" value="1" />
+    Name:<input type="text" name="name" /><br>
+    Email:<input type="text" name="mail" /><br>
+    Phone:<input type="text" name="phone" /><br>
+    <input type="submit" value="Add" />
+</form>
+<?php
  }
 else
 {
--- adressbook.php
+++ adressbook.php
@@ -2,5 +2,27 @@
 
 echo "<a href='logout.php'>Logout</a> | <a href='add.php'>Add</a>";
 
+$people = file('data.txt');
+echo "<table border='1'>"
+ . "<tr><td>Name</td><td>Email</td><td>Phone</td></tr>";
+
+foreach ($people as $value) {
+    $tmp = explode(';', $value);
+    foreach ($tmp as $v) {
+        $tmp2 = explode(':', $v);
+        if ($tmp2[0] == 'name') {
+            $name = $tmp2[1];
+        } elseif ($tmp2[0] == 'email') {
+            $email = $tmp2[1];
+        } elseif ($tmp2[0] == 'phone') {
+            $phone = $tmp2[1];
+        }
+        
+    }
+    echo "<tr><td>" . $name . "</td><td>" . $email . "</td><td>" . $phone . "</td></tr>";
+}
+
+echo "</table>";
+
 
 
