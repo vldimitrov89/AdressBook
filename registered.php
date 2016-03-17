742871f8942b62638c628caca557b22e58a735a4
index-panayotov <index-panayotov@users.noreply.github.com>
2016-03-17 11:13:55 +0200

    changing logic for login

--- registered.php
+++ registered.php
@@ -4,14 +4,12 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username == 'root' && $password == '12345')
-{
-    $_SESSION['is_Logged'] = true;
-    header('Location: index.php');
-}
-else
+if ($username != 'root' && $password != '12345')
 {
     echo "Your Username and Password are invalid!<br>";
     echo "<a href='login.php'>Try Again</a>";
+    die();
 }
 
+ $_SESSION['is_Logged'] = true;
+    header('Location: index.php');
