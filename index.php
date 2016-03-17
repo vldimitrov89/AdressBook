ede1f6d4ec9ba2d386b4a75660aef695a6896376
index-panayotov <index-panayotov@users.noreply.github.com>
2016-03-17 11:17:58 +0200

    cleaning up  index.php
    ne trqbva da imash html predi header function-a

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
-        session_start();
-        ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    </head>
-    <body>
-        <?php
-        if($_SESSION['is_Logged']==true)
-        {
-            header('Location: adressbook.php');
-        }
-        else
-        {
-            header('Location: login.php');
-        }
-        ?>
-    </body>
-</html>
+session_start();
+
+$link = ($_SESSION['is_Logged']==true)? "addressbook.php" : "login.php";
+
+header('Location: $link');
